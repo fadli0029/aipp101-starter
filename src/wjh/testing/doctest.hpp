722fc785bf2e2e6869712407bb37bffbd2c22d98
fadli0029 <fadlialim0029@gmail.com>
2026-02-27 15:11:08 -0800

    Fix GCC 15 ciso646 warning in doctest wrapper

--- src/wjh/testing/doctest.hpp
+++ src/wjh/testing/doctest.hpp
@@ -12,6 +12,18 @@
 #define DOCTEST_CONFIG_NO_SEED_IN_INTRO_DEFAULT false
 #define DOCTEST_CONFIG_INITIALIZE_RAND_SEED
 
+#if defined(__clang__)
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-W#warnings"
+#elif defined(__GNUC__)
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wcpp"
+#endif
 #include <doctest/doctest.h>
+#if defined(__clang__)
+#    pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#    pragma GCC diagnostic pop
+#endif
 
 #endif // WJH_CHAT_2804E4281F4047E684D3C4405141CF62
